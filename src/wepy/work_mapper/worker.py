e5674f75a154b2690559bea5a33d67cc7d64f1b6
Samuel Lotz <sam@examol.io>
2025-11-24 16:23:29 -0500

    get tests passing

--- src/wepy/work_mapper/worker.py
+++ src/wepy/work_mapper/worker.py
@@ -1,16 +1,8 @@
 """Classes for workers and tasks for use with WorkerMapper."""
 
-# Standard Library
-import logging
+from .mapper import Worker, WorkerMapper
 
-logger = logging.getLogger(__name__)
-# Standard Library
-
-# First Party Library
-# we can't move the WorkerMapper here until some of the pickles I have
-# laying around don't expect it to be here. In the meantime, new
-# software can expect it to be here so we import it here.
-
-# this whole thing should get refactored into a better name which
-# should be something like ConsumerMapper because our workers act like
-# consumers
+__all__ = [
+    "Worker",
+    "WorkerMapper",
+]
