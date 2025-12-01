3a9f3fdd22c807bb48ffb07198a286de795ce459
Samuel Lotz <sam@examol.io>
2025-12-01 10:48:15 -0500

    initial implementation of proc pool mapper

--- src/wepy/work_mapper/proc_pool_mapper.py
+++ src/wepy/work_mapper/proc_pool_mapper.py
@@ -11,16 +11,6 @@ logger = logging.getLogger(__name__)
 
 class ProcPoolMapper:
 
-    num_workers: int
-
-    def __init__(
-            self,
-            num_workers: int,
-            proc_start_method: Literal["fork", "spawn", "forkserver"] = "spawn",
-    ) -> None:
-        self.num_workers = num_workers
-        self.proc_start_method = proc_start_method
-
     def init(
             self,
             segment_func: Callable[
@@ -31,14 +21,40 @@ class ProcPoolMapper:
                 ],
                 AnyWalkerState,
             ],
+            num_workers: int,
+            worker_args: list[dict[str, Any]] | None = None,
+            proc_start_method: Literal["fork", "spawn", "forkserver"] = "spawn",
     ) -> None:
+        """l..
+
+        worker_func_args: This is an arbitrary set of key-values that
+            for each worker will be passed into the segment_func call
+            if that worker is used. Useful for injecting things like
+            device IDs.
+
+        """
 
         logger.info("Initializing ProcPoolMapper")
 
+        self._num_workers = num_workers
+        self._proc_start_method = proc_start_method
+
+        if worker_args is not None and len(worker_args) != num_workers:
+            raise ValueError("If worker_args are given they must match the number of workers.")
+
+        elif worker_args is None:
+            logger.info("No worker arguments given.")
+            self._worker_args = [{} for _ in range(self._num_workers)]
+
+        else:
+            logger.info(f"Configured workers with the following function arguments: {worker_args}")
+            self._worker_args = worker_args
+            
+
         self._func = segment_func
 
-        logger.info(f"Initializing local multiprocessing context with start method: {self.proc_start_method}")
-        self._mp_ctx = mp.get_context(method=self.proc_start_method)
+        logger.info(f"Initializing local multiprocessing context with start method: {self._proc_start_method}")
+        self._mp_ctx = mp.get_context(method=self._proc_start_method)
 
     def cleanup(self) -> None:
 
@@ -51,12 +67,12 @@ class ProcPoolMapper:
             *args: list[list[Any]],
     ) -> list[AnyWalkerState]:
 
-        logger.info(f"Running map on {len(walker_states)} in batches of {self.num_workers}")
+        logger.info(f"Running map on {len(walker_states)} in batches of {self._num_workers}")
 
         # spin up a new pool for each map
-        logger.info(f"Starting process Pool with {self.num_workers}")
+        logger.info(f"Starting process Pool with {self._num_workers}")
         with self._mp_ctx.Pool(
-                processes=self.num_workers,
+                processes=self._num_workers,
                 # only run one thing per task, just to make sure
                 # everything is cleaned up
                 maxtasksperchild=1,
@@ -65,7 +81,7 @@ class ProcPoolMapper:
             results = []
             for batch_idx, batch in enumerate(itertools.batched(
                     zip(walker_states, *args, strict=True),
-                    self.num_workers,
+                    self._num_workers,
                     strict=False,
             )):
 
@@ -75,11 +91,15 @@ class ProcPoolMapper:
                 for batch_task_idx, batch_args in enumerate(batch):
 
                     task_idx = batch_idx + batch_task_idx
+                    # for our purposes each element in this batch
+                    # should be associated with a worker.
+                    worker_idx = batch_task_idx
 
-                    logger.info(f"Submitting task {task_idx}")
+                    logger.info(f"Submitting task {task_idx} to worker {worker_idx}")
                     result = pool.apply_async(
                         self._func,
-                        batch_args,
+                        args=batch_args,
+                        kwds=self._worker_args[worker_idx],
                     )
                     logger.info(f"Task {task_idx} submitted")
                     batch_results.append(result)
--- tests/unit/test_work_mapper/test_proc_pool_mapper.py
+++ tests/unit/test_work_mapper/test_proc_pool_mapper.py
@@ -24,30 +24,9 @@ def test_rizz_walker():
 
 def test_ProcPoolMapper():
 
-    poolmapper = ProcPoolMapper(num_workers=1)
+    poolmapper = ProcPoolMapper()
 
-    poolmapper.init(rizz_run)
-
-    results = poolmapper.map(
-            [
-                RizzWalkerState(1),
-                RizzWalkerState(1),
-                RizzWalkerState(2),
-            ],
-            [1, 2, 2],
-            [2, 2, 2],
-    )
-
-    assert results == [
-            RizzWalkerState(4),
-            RizzWalkerState(6),
-            RizzWalkerState(8),
-        ]
-
-
-    poolmapper = ProcPoolMapper(num_workers=2)
-
-    poolmapper.init(rizz_run)
+    poolmapper.init(rizz_run, num_workers=1)
 
     results = poolmapper.map(
             [
@@ -65,9 +44,30 @@ def test_ProcPoolMapper():
             RizzWalkerState(8),
         ]
 
-    poolmapper = ProcPoolMapper(num_workers=3)
 
-    poolmapper.init(rizz_run)
+    poolmapper = ProcPoolMapper()
+
+    poolmapper.init(rizz_run, num_workers=2)
+
+    results = poolmapper.map(
+            [
+                RizzWalkerState(1),
+                RizzWalkerState(1),
+                RizzWalkerState(2),
+            ],
+            [1, 2, 2],
+            [2, 2, 2],
+    )
+
+    assert results == [
+            RizzWalkerState(4),
+            RizzWalkerState(6),
+            RizzWalkerState(8),
+        ]
+
+    poolmapper = ProcPoolMapper()
+
+    poolmapper.init(rizz_run, num_workers=3)
 
     results = poolmapper.map(
             [
@@ -85,9 +85,9 @@ def test_ProcPoolMapper():
             RizzWalkerState(8),
         ]
 
-    poolmapper = ProcPoolMapper(num_workers=3, proc_start_method="fork")
+    poolmapper = ProcPoolMapper()
 
-    poolmapper.init(rizz_run)
+    poolmapper.init(rizz_run, num_workers=3, proc_start_method="fork")
 
     results = poolmapper.map(
             [
@@ -104,3 +104,23 @@ def test_ProcPoolMapper():
             RizzWalkerState(6),
             RizzWalkerState(8),
         ]
+
+    # use worker args to fill in one of the args
+    poolmapper = ProcPoolMapper()
+
+    poolmapper.init(
+        rizz_run,
+        num_workers=2,
+        worker_args=[
+            {"multiple" : 2},
+            {"multiple" : 3},
+        ])
+
+    results = poolmapper.map(
+            [
+                RizzWalkerState(1),
+                RizzWalkerState(1),
+                RizzWalkerState(1),
+            ],
+            [1, 1, 1],
+    )
