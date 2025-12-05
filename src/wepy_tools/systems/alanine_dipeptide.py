bd50847c0fe46982bc93c574c862cc90fa694755
Samuel Lotz <sam@examol.io>
2025-12-05 12:29:02 -0500

    add alanine dipeptide ramachandran distance metric

--- src/wepy_tools/systems/alanine_dipeptide.py
+++ src/wepy_tools/systems/alanine_dipeptide.py
@@ -1,8 +1,17 @@
+from typing import Any
 from importlib.resources import files
 
+import attrs
+import numpy as np
 import openmm
 import openmm.app
 import openmm.unit
+import mdtraj
+
+from wepy.walker import WalkerState
+from wepy.runners.openmm import OpenMMState
+from wepy.resampling.distances.distance import Distance
+from wepy.util.mdtraj import traj_fields_to_mdtraj
 
 DEFAULT_EWALD_ERROR_TOLERANCE = 1.0e-5
 DEFAULT_CUTOFF_DISTANCE = 10.0 * openmm.unit.angstroms
@@ -48,7 +57,7 @@ class AlanineDipeptideExplicit:
             hydrogenMass=None,
             switch_width=DEFAULT_SWITCH_WIDTH,
             ewaldErrorTolerance=DEFAULT_EWALD_ERROR_TOLERANCE,
-    ):
+    ) -> None:
 
         prmtop_filename = files("wepy_tools.systems.data.alanine_dipeptide_explicit") / "alanine-dipeptide.prmtop"
         crd_filename = files("wepy_tools.systems.data.alanine_dipeptide_explicit") / "alanine-dipeptide.crd"
@@ -84,7 +93,79 @@ class AlanineDipeptideExplicit:
         positions = inpcrd.getPositions(asNumpy=True)
 
         # Set box vectors.
-        box_vectors = inpcrd.getBoxVectors(asNumpy=True)
-        system.setDefaultPeriodicBoxVectors(box_vectors[0], box_vectors[1], box_vectors[2])
+        _box_vectors = inpcrd.getBoxVectors(asNumpy=True)
+        system.setDefaultPeriodicBoxVectors(_box_vectors[0], _box_vectors[1], _box_vectors[2])
+
+
+        self.box_vectors = np.array(
+            [
+                vec_q.value_in_unit(vec_q.unit)
+                for vec_q
+                in _box_vectors
+            ]
+        ) * _box_vectors[0].unit
 
         self.system, self.positions = system, positions
+
+@attrs.define
+class AlanineDipeptideRamachandranDistanceImage(WalkerState):
+
+    phis: np.typing.ArrayLike
+    psis: np.typing.ArrayLike
+
+@attrs.define
+class AlanineDipeptideRamachandranDistance(Distance):
+    # the parsed JSON topology of plain python objects
+    topology: str
+
+    def image(self, state: OpenMMState) -> AlanineDipeptideRamachandranDistanceImage:
+
+
+        _unit = state["positions"].unit
+        state_dict = {
+            # traj shape to match interface requirements
+            key : np.array([quantity.value_in_unit(_unit)])
+            for key, quantity
+            in state.to_dict().items()
+            if key in {"positions", "box_vectors"}
+        }
+        traj = traj_fields_to_mdtraj(
+            state_dict,
+            self.topology,
+        )
+
+        is_periodic = "box_vectors" in state
+
+        _, phis = mdtraj.compute_phi(
+            traj,
+            periodic=is_periodic,
+            opt=True,
+        )
+        _, psis = mdtraj.compute_psi(
+            traj,
+            periodic=is_periodic,
+            opt=True,
+        )
+
+        return AlanineDipeptideRamachandranDistanceImage(
+            phis=phis,
+            psis=psis,
+        )
+
+
+    def image_distance(
+            self,
+            image_a: AlanineDipeptideRamachandranDistanceImage,
+            image_b: AlanineDipeptideRamachandranDistanceImage,
+    ) -> float:
+
+        angles_a = np.concatenate((image_a.phis, image_a.psis))
+        angles_b = np.concatenate((image_b.phis, image_b.psis))
+
+        # compute the circular difference
+        deltas = np.atan2(
+            np.sin(angles_a - angles_b),
+            np.cos(angles_a - angles_b),
+        )
+
+        return np.sqrt(np.sum(deltas**2))
--- tests/unit/test_wepy_tools/test_systems/test_alanine_dipeptide.py
+++ tests/unit/test_wepy_tools/test_systems/test_alanine_dipeptide.py
@@ -1,5 +1,86 @@
-from wepy_tools.systems.alanine_dipeptide import AlanineDipeptideExplicit
+import json
+
+import numpy as np
+import mdtraj
+
+from wepy.util.mdtraj import mdtraj_to_json_topology
+from wepy.runners.openmm import OpenMMState
+from wepy_tools.systems.alanine_dipeptide import AlanineDipeptideExplicit, AlanineDipeptideRamachandranDistance
+
 
 def test_AlanineDipeptideExplicit():
 
     AlanineDipeptideExplicit()
+
+class Test_AlanineDipeptideRamachandranDistance:
+
+    def test_image(self):
+
+        ala_sys = AlanineDipeptideExplicit()
+
+        json_top = mdtraj_to_json_topology(
+                mdtraj.Topology.from_openmm(ala_sys.topology)
+            )
+
+        distance = AlanineDipeptideRamachandranDistance(topology=json_top)
+
+        state = OpenMMState.from_dwim(
+            positions=ala_sys.positions,
+            box_vectors=ala_sys.box_vectors,
+        )
+
+        image = distance.image(state)
+
+    def test_image_distance(self):
+
+        ala_sys = AlanineDipeptideExplicit()
+
+        json_top = mdtraj_to_json_topology(
+                mdtraj.Topology.from_openmm(ala_sys.topology)
+            )
+
+        distance = AlanineDipeptideRamachandranDistance(topology=json_top)
+
+        state = OpenMMState.from_dwim(
+            positions=ala_sys.positions,
+            box_vectors=ala_sys.box_vectors,
+        )
+
+        image_a = distance.image(state)
+
+        assert np.isclose(distance.image_distance(image_a, image_a), 0.)
+
+        # then make a jittered atom positions to get something a little
+        # different to compare
+        jitter_positions = ala_sys.positions + np.random.uniform(
+            -0.01,
+            0.01,
+            size=ala_sys.positions.shape,
+        ) * ala_sys.positions.unit
+
+        jitter_state = OpenMMState.from_dwim(
+            positions=jitter_positions,
+            box_vectors=ala_sys.box_vectors,
+        )
+
+        jitter_image = distance.image(jitter_state)
+
+        assert not np.isclose(
+            distance.image_distance(
+                image_a,
+                jitter_image,
+            ),
+            0.,
+        )
+
+        # test it is symmetric
+        assert np.isclose(
+            distance.image_distance(
+                image_a,
+                jitter_image,
+            ),
+            distance.image_distance(
+                jitter_image,
+                image_a,
+            ),
+        )
