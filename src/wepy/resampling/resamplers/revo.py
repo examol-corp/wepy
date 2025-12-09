96c9d03794ea94ac6c746e8c8dd658ac0a099eac
Samuel Lotz <sam@examol.io>
2025-12-08 23:32:38 -0500

    use spawn for revo multiprocessing Pool
    using fork can cause deadlocks in subprocesses and reliability is key
    so we stop that.

--- src/wepy/resampling/resamplers/revo.py
+++ src/wepy/resampling/resamplers/revo.py
@@ -129,7 +129,6 @@ class REVOResampler(CloneMergeResampler):
         merge_dist=None,
         char_dist=None,
         distance=None,
-        init_state=None,
         weights=True,
         merge_alg="pairs",
         pmin=1e-12,
@@ -202,7 +201,6 @@ class REVOResampler(CloneMergeResampler):
         assert merge_dist is not None, "Merge distance must be given."
         assert distance is not None, "Distance object must be given."
         assert char_dist is not None, "Characteristic distance value (d0) must be given"
-        assert init_state is not None, "An initial state must be given."
 
         # ln(probability_min)
         self.lpmin = np.log(self.pmin / 100)
@@ -660,8 +658,11 @@ class REVOResampler(CloneMergeResampler):
             handlers = list(logging.getLogger().handlers)
             listener = logging.handlers.QueueListener(log_queue, *handlers)
             listener.start()
-            
-            with mp.Pool(
+
+            # NOTE: Must use spawn here, otherwise there are problems
+            # with deadlocking in the sub-processes
+            mp_ctx = mp.get_context(method="spawn")
+            with mp_ctx.Pool(
                     self.num_proc,
                     initializer=proc_pool_worker_setup,
                     initargs=(log_queue,),
